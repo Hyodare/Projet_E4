6f614c123199266d792b28a04b0c41b54ffdd100
alecto <gui-u@users.noreply.github.com>
2018-04-10 23:07:40 +0200

    Last tuesday commit

--- arduino/communicationSerieSimple/communicationSerieSimple.ino
+++ arduino/communicationSerieSimple/communicationSerieSimple.ino
@@ -1,13 +1,9 @@
-/*
 
- 
- 
- */
-
+#define usleep 10000
 // constants won't change. They're used here to 
 // set pin numbers:
-const int txPin = 1;
-const int rxPin = 0;
+const int txPin = 13;
+const int rxPin = 12;
 const int d0Pin = 2; //data pin 0
 const int d1Pin = 3;
 const int d2Pin = 4;
@@ -26,6 +22,8 @@ int appareillage1=0;
 int acknow=0;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.print("Begin OK\n");
   pinMode(d0Pin, OUTPUT);
   pinMode(d1Pin, OUTPUT);
   pinMode(d2Pin, OUTPUT);
@@ -38,12 +36,29 @@ void setup() {
   pinMode(c1Pin, OUTPUT);
   pinMode(rxPin, INPUT);      
   pinMode(txPin, OUTPUT);
+  
+  //init
+//  digitalWrite(rxPin, LOW);
+  digitalWrite(txPin, LOW);
+  digitalWrite(d0Pin, LOW);
+  digitalWrite(d1Pin, LOW);
+  digitalWrite(d2Pin, LOW);
+  digitalWrite(d3Pin, LOW);
+  digitalWrite(d4Pin, LOW);
+  digitalWrite(d5Pin, LOW);
+  digitalWrite(d6Pin, LOW);
+  digitalWrite(d7Pin, LOW);
+  digitalWrite(c0Pin, LOW);
+  digitalWrite(c1Pin, LOW);
+  delay(1000);
+  Serial.print("Setup OK\n");
 }
 
 void loop(){
 
   if (initOK ==0)
   {
+    Serial.print("Debut init\n");
     // read the state of the rx entry:
     rxState = digitalRead(rxPin);
     if (rxState == HIGH) {     
@@ -55,14 +70,16 @@ void loop(){
           digitalWrite(txPin, LOW);
           appareillage1=1;
           initOK=1;
+          Serial.print("Init OK\n");
         }
       }  
     }
   }
   else // System initialised
   {
+    Serial.print("Jattends\n");
     if (rxState == HIGH) {
-
+      Serial.print("Je demarre\n");
       digitalWrite(d0Pin, HIGH); //Bit de poids faible
       digitalWrite(d1Pin, LOW);
       digitalWrite(d2Pin, LOW);
@@ -73,15 +90,18 @@ void loop(){
       digitalWrite(d7Pin, HIGH);
       digitalWrite(c0Pin, LOW); //canal part 0
       digitalWrite(c1Pin, HIGH);//canal part 1
-      delayMicroseconds(100);
+      delayMicroseconds(usleep);
+      Serial.print("Je me reveille\n");
       digitalWrite(txPin, HIGH);// write 1 to tx.
       while(acknow==0) //wait ack from rx
       {
+        Serial.print("J'attends la reponse\n");
         rxState = digitalRead(rxPin);
         if (rxState == LOW) { //ack ok --> write 0 to tx
           digitalWrite(txPin, LOW);
           acknow=1;
           initOK=1;
+          Serial.print("Fini\n");
 
         }
       }
@@ -89,6 +109,7 @@ void loop(){
 
   }
 }
+
 
 
 
