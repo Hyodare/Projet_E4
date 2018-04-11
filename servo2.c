6235401f728684a5180e743bfe1a587812631824
Hyodare <francois.pierrick@outlook.fr>
2018-04-11 17:48:03 +0200

    mvts

--- servo2.c
+++ servo2.c
@@ -175,6 +175,18 @@ int main(int argc, char* argv[])
 		usleep(1500000);
 		sprintf(fichier.nom,"fuck");
 		sem_post(fichier.change);
+		usleep(1500000);
+		sprintf(fichier.nom,"ouvre");
+		sem_post(fichier.change);
+		usleep(1500000);
+		sprintf(fichier.nom,"peace");
+		sem_post(fichier.change);
+		usleep(1500000);
+		sprintf(fichier.nom,"ouvre");
+		sem_post(fichier.change);
+		usleep(1500000);
+		sprintf(fichier.nom,"pouce");
+		sem_post(fichier.change);
 	}
 	usleep(5000000);
 	sprintf(fichier.nom,"ouvre");
