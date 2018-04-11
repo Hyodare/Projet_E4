bab5ae337d895da3318f1ea00a2cd8a014ab78e1
Hyodare <francois.pierrick@outlook.fr>
2018-04-11 18:47:57 +0200

    mvts

--- servo2.c
+++ servo2.c
@@ -163,34 +163,34 @@ int main(int argc, char* argv[])
 	}*/
 	while(1)
 	{
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"ouvre");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"yolo");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"ouvre");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"spiderman");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"ouvre");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"fuck");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"ouvre");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"peace");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"ouvre");
 		sem_post(fichier.change);
-		usleep(5000000);
+		usleep(1500000);
 		sprintf(fichier.nom,"pouce");
 		sem_post(fichier.change);
 	
